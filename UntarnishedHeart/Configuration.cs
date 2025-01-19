5d36befcf7b44d640c1805a262816eb2cbd3804b
VeeverSW <veever233@outlook.com>
2025-01-19 22:36:43 +0800

    feat: 增加四人本测试预设

--- UntarnishedHeart/Configuration.cs
+++ UntarnishedHeart/Configuration.cs
@@ -33,13 +33,24 @@ public class Configuration : IPluginConfiguration
                                                    new() { DataID = 245, Note = "Note2", Position = new(-0.63603175f, -1.8021163f, 0.6449276f), Delay = 5000, StopInCombat = false}]
     };
 
+    public static readonly ExecutorPreset ExamplePreset3 = new()
+    {
+        Name = "黄金谷四人本 (测试用)", Zone = 172, Steps = [new() { DataID = 1471, Note = "Boss1", Position = new (31.613f, -9.256f, 3.126f), Delay = 5000, StopInCombat = true},
+                                                   new() { DataID = 245, Note = "Boss1后魔界花", Position = new(-37.391f, -17.294f, -94.228f), Delay = 5000, StopInCombat = true},
+                                                   new() { DataID = 1470, Note = "Boss2", Position = new(-166.161f, -29.802f, -141.621f), Delay = 5000, StopInCombat = true },
+                                                   new() { Note = "动画", Position = new(-373.036f, -32.154f, -137.851f), Delay = 5000, StopInCombat = true },
+                                                   new() { DataID = 943, Note = "Boss3", Position = new(-410.875f, -33.293f, -126.703f), Delay = 5000, StopInCombat = true }]
+    };
+
     public void Init()
     {
-        if (Presets.Count == 0)
+        if (Presets.Count < 3)
         {
+            Presets.Clear();
             Presets.Add(ExamplePreset0);
             Presets.Add(ExamplePreset1);
             Presets.Add(ExamplePreset2);
+            Presets.Add(ExamplePreset3);
             Save();
         }
     }
