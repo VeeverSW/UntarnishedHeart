71f03bb9bc46371f1873b7c4a4f0c9a11bb81e7d
VeeverSW <veever233@outlook.com>
2025-01-19 23:56:45 +0800

    feat: try handle Treasure

--- UntarnishedHeart/Executor/Executor.cs
+++ UntarnishedHeart/Executor/Executor.cs
@@ -8,6 +8,9 @@ using Dalamud.Game.ClientState.Objects.Enums;
 using Lumina.Excel.GeneratedSheets;
 using UntarnishedHeart.Managers;
 using UntarnishedHeart.Utils;
+using System.Collections.Generic;
+using System.Numerics;
+using Dalamud.Game.ClientState.Objects.Types;
 
 namespace UntarnishedHeart.Executor;
 
@@ -20,6 +23,9 @@ public class Executor : IDisposable
     public ExecutorPreset? ExecutorPreset   { get; init; }
     public string          RunningMessage   => TaskHelper?.CurrentTaskName ?? string.Empty;
     public bool            IsDisposed       { get; private set; }
+
+    public List<Vector3> TreasureRecordList = new List<Vector3>(); 
+    public List<IGameObject> Treasures = new List<IGameObject>();
 
     private TaskHelper? TaskHelper;
 
@@ -99,7 +105,26 @@ public class Executor : IDisposable
         }, "等待副本开始");
 
         foreach (var task in ExecutorPreset.GetTasks(TaskHelper, Service.Config.MoveType))
+        {
             task.Invoke();
+            TaskHelper.Enqueue(() =>
+            {
+            var treasures = DService.ObjectTable
+                    .Where(obj => obj.ObjectKind == ObjectKind.Treasure)
+                    .ToList();
+            if (treasures.Count == 0) return;
+
+                foreach (var obj in treasures)
+                {
+                    if (!TreasureRecordList.Contains(obj.Position))
+                    {
+                        Treasures.Add(obj);
+                        TreasureRecordList.Add(obj.Position);
+                        Chat($"add: obj:{obj}, pos: {obj.Position}");
+                    }
+                }
+            });
+        }
     }
 
     // 副本完成
@@ -177,26 +202,52 @@ public class Executor : IDisposable
 
         TaskHelper.Enqueue(() =>
         {
-            var treasures = DService.ObjectTable
-                                    .Where(obj => obj.ObjectKind == ObjectKind.Treasure)
-                                    .ToList();
-            if (treasures.Count == 0) return false;
-
-            foreach (var obj in treasures)
+            //var treasures = DService.ObjectTable
+            //                        .Where(obj => obj.ObjectKind == ObjectKind.Treasure)
+            //                        .ToList();
+            //if (treasures.Count == 0) return false;
+            var inCount = 0;
+            foreach (var obj in Treasures)
             {
                 TaskHelper.Enqueue(() => GameFunctions.Teleport(obj.Position), "传送至宝箱", null, null, 2);
                 TaskHelper.DelayNext(setDelayTime, "等待位置确认", false, 2);
-                TaskHelper.Enqueue(() =>
-                {
-                    if (!Throttler.Throttle("交互宝箱节流")) return false;
-                    return TargetSystem.Instance()->InteractWithObject(obj.ToStruct(), false) != 0;
-                }, "与宝箱交互", null, null, 2);
+                Chat($"count: {inCount}");
+                //TaskHelper.Enqueue(() =>
+                //{
+                //    if (!Throttler.Throttle("交互宝箱节流")) return false;
+                //    obj.Interact();
+                //    return TargetSystem.Instance()->InteractWithObject(obj.ToStruct(), false) != 0;
+                //}, "与宝箱交互", null, null, 2);
+                TaskHelper.Enqueue(() => obj.Interact(), "交互宝箱节流", null, null, 2);
+                TaskHelper.DelayNext(setDelayTime, "等待位置确认", false, 5000);
+                inCount++;
             }
 
             return true;
         }, "搜寻宝箱中");
 
         TaskHelper.Enqueue(() => GameFunctions.Teleport(origPosition), "传送回原始位置");
+        //TaskHelper.Enqueue(() =>
+        //{
+        //    Chat($"TreasureRecordListCount: {TreasureRecordList.Count}");
+        //    foreach (var obj in Treasures)
+        //    {
+        //        TaskHelper.Enqueue(() => GameFunctions.Teleport(obj.Position), "传送至宝箱", null, null, 2);
+        //        TaskHelper.DelayNext(setDelayTime, "等待位置确认", false, 2);
+        //        TaskHelper.Enqueue(() =>
+        //        {
+        //            if (!Throttler.Throttle("交互宝箱节流")) return false;
+        //            return TargetSystem.Instance()->InteractWithObject(obj.ToStruct(), false) != 0;
+
+        //        }, "与宝箱交互", null, null, 2);
+
+        //        TaskHelper.DelayNext(setDelayTime, "等待位置确认", false, 2000);
+        //    }
+
+        //    return true;
+        //}, "搜寻宝箱中");
+
+        //TaskHelper.Enqueue(() => GameFunctions.Teleport(origPosition), "传送回原始位置");
     }
 
     // 放弃先前任务
